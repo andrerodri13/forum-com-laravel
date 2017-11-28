09d1d255a85ade980004bfc3214089e0d9905a47
André S. Rodrigues <andre@asrsistemas.com.br>
2017-11-28 20:13:41 -0200

    layout do forum com traducao

---
+++ resources/views/layouts/default/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="page-footer">
+    <div class="container">
+        &copy; copyright {{ date('Y') }} <a href="http://asrsistemas.com.br" class="grey-text text-lighten-4 right" target="_blank">ASR Sistemas</a> 
+    </div>
+</footer>
